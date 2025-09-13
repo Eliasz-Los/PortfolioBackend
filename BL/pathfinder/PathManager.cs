17a24624957aecafe822bdf9c7cc7e1625823345
Eliasz Los <eliasz.los@student.kdg.be>
2025-09-14 00:11:14 +0200

    Start of the awesome portfolio project

---
+++ BL/pathfinder/PathManager.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using Microsoft.Extensions.Logging;
+
+namespace BL.pathfinder;
+
+public class PathManager
+{
+    private readonly ILogger<PathManager> _logger;
+
+    public PathManager(ILogger<PathManager> logger)
+    {
+        _logger = logger;
+    }
+    
+    //TODO implement floorplan repository
+    public async Task<List<Point>> FindPath(Point start, Point end, String folderPath, String hospitalName, int floorNumber)
+    {
+        //Getting the image path
+        //TODO
+        // var floorplan = _floorplanRepository.ReadFloorplanByHospitalNameAndFloorNumber(hospitalName, floorNumber);
+        // var imagePath = Path.Combine(folderPath, floorplan?.Image);
+        //Analyzing the walkable points
+        // var (startP, endP, walkablePoints) = await Task.Run( () => FloorplanAnalyzer.GetWalkablePoints(imagePath, start ,end));
+        // if (walkablePoints.Count == 0)
+        // {
+        //     _logger.LogError("No walkable points found.");
+        //     return new List<Point>();
+        // }
+        // //Navigating the path
+        // var path = await Task.Run(() => AStarPathfinding.FindPath(startP, endP, walkablePoints));
+        // //var path = await Task.Run(() => AStarBidirectional.FindPathBidirectional(startP, endP, walkablePoints));
+        // _logger.LogInformation($"Path found: {path} {path.Count}");
+        return new List<Point>();
+    }
+}
---
+++ Domain/pathfinder/Point.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Domain;
+
+public class Point
+{
+    public Guid Id {get; set;}
+    public double XWidth {get; set;}
+    public double YHeight {get; set;}
+    public Floorplan Floorplan {get; set;}
+    
+    public Point(){}
+    public Point(double xWidth, double yHeight)
+    {
+        XWidth = xWidth;
+        YHeight = yHeight;
+    }
+
+    public Point(double xWidth, double yHeight, Floorplan floorplan)
+    {
+        XWidth = xWidth;
+        YHeight = yHeight;
+        Floorplan = floorplan;
+    }
+
+    public bool Equals(Point? other)
+    {
+        if (other is null) return false;
+        const double tolerance = 1e-10;
+            return Math.Abs(XWidth - other.XWidth) < tolerance
+                   && Math.Abs(YHeight - other.YHeight) < tolerance;
+       
+    }
+
+    public override bool Equals(object? obj) => obj is Point other && Equals(other);
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(XWidth, YHeight);
+    }
+}
