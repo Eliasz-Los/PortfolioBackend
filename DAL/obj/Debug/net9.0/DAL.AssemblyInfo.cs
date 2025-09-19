303c31cfd2b05ed9e1db66ac8384eaf355594815
Eliasz-Los <kmzeliasz@gmail.com>
2025-09-19 17:03:06 +0200

    cors simplified

--- DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ef3ca0e28edf90009333525a528136c2a82fda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6fbee0e811597498ab70ad167ba9e2cdf8127d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
