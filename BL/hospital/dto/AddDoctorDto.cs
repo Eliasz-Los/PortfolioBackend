f5d50998ff2cbd1861f5959ce236cd42b8efd116
Eliasz-Los <kmzeliasz@gmail.com>
2025-12-28 01:58:30 +0100

    added the features, tested the getdoctorbyid

---
+++ BL/hospital/dto/AddDoctorDto.cs
@@ -0,0 +1,10 @@
+﻿using Domain.hospital.types;
+
+namespace BL.hospital.dto;
+
+public class AddDoctorDto
+{
+    public Name FullName { get; set; }
+    public Specialisation Specialisation { get; set; }
+    public Location WorkAddress { get; set; }
+}
